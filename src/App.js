adcf401c802bb51dde05d62d377c95a5488c29c3
mishelramirez <rmishel5647@gmail.com>
2022-09-19 21:16:30 -0500

    se realiza navbar y cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Inicio from "./componentes/Inicio";
+import Cards from "./componentes/Cards";
+
 function App() {
-  return <div>Holi Mundo</div>;
+  return <div>
+  < Inicio/>
+  <Cards/>
+  </div>
 }
 
 export default App;
